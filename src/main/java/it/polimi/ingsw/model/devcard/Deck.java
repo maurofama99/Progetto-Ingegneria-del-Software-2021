0a4574db1c31afc2176676995edd399e7d4f83db
maurofama99 <mauro.fama@mail.polimi.it>
2021-05-02 11:41:37 +0200

    Deck constructor created
    When Deck class is created, Gson parser initialize the arraylist of devCards from JSON file

--- src/main/java/it/polimi/ingsw/model/devcard/Deck.java
+++ src/main/java/it/polimi/ingsw/model/devcard/Deck.java
@@ -1,5 +1,12 @@
 package it.polimi.ingsw.model.devcard;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
 import java.util.Stack;
 import java.util.ArrayList;
 
@@ -15,13 +22,28 @@ public class Deck {
 
     private ArrayList<DevCard> generalDeck;
 
-    //inizializzazione del deck con gson
+    //when Deck class is created, generalDeck is already setted with all the devCards
+    public Deck() {
+        Gson gson = new Gson();
+
+        try (Reader reader = new FileReader("src/main/resources/DevelopmentCards.json")) {
+
+            Type devCardListType = new TypeToken<ArrayList<DevCard>>(){}.getType();
+
+            generalDeck = gson.fromJson(reader, devCardListType);
+
+        } catch (IOException e) {
+            System.out.println("Can't find DevelopmentCards.json path");
+            e.printStackTrace();
+        }
+    }
+
 
     /**
      * This method is literally a getter of the deck on the table
      * @return the matrix of the cards still on the table (not taken by players or removed)
      */
-    public DevCard[][] ShowedDeck(){
+    public DevCard[][] ShowedCards(){
 
         DevCard[][] result = new DevCard[3][4];
 
@@ -40,7 +62,6 @@ public class Deck {
      * @return the card selected if present
      */
     public DevCard getDevCard(int row, int column){
-        
         return fullDeck[row-1][column-1].peek();
     }
 
@@ -51,7 +72,6 @@ public class Deck {
      * @return a pop of the deck, removing the selected card (the top one)
      */
     public DevCard removeAndGetCard(int row, int col){
-
         return fullDeck[row-1][col-1].pop();
     }
 
