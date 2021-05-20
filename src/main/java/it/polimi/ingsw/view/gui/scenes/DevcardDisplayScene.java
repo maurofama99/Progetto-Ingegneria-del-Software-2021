97d70ba0e136bb900241312dc2b41cb1c3c9d9c7
Christian Fabio Grazian <10629810@polimi.it>
2021-05-20 12:45:30 +0200

    Updated View
    Personal Board, Market and Dev Card Deck in progress

--- src/main/java/it/polimi/ingsw/view/gui/scenes/DevcardDisplayScene.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/DevcardDisplayScene.java
@@ -1,4 +1,10 @@
 package it.polimi.ingsw.view.gui.scenes;
 
-public class DevcardDisplayScene implements GenericSceneController{
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class DevcardDisplayScene implements GenericSceneController {
+    @FXML
+    private GridPane devCardGrid;
+
 }
--- src/main/java/it/polimi/ingsw/view/gui/scenes/PersonalBoardSceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/PersonalBoardSceneController.java
@@ -1,4 +1,38 @@
 package it.polimi.ingsw.view.gui.scenes;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class PersonalBoardSceneController implements GenericSceneController {
+    @FXML
+    private ImageView firstLevelLeft;
+    @FXML
+    private ImageView firstLevelCenter;
+    @FXML
+    private ImageView firstLevelRight;
+    @FXML
+    private ImageView secondLevelLeft;
+    @FXML
+    private ImageView secondLevelRight;
+    @FXML
+    private ImageView thirdLevel;
+    @FXML
+    private ImageView faithCross;
+    @FXML
+    private ImageView slotLeft;
+    @FXML
+    private ImageView slotCenter;
+    @FXML
+    private ImageView slotRight;
+    @FXML
+    private ImageView firstPopeTile;
+    @FXML
+    private ImageView secondPopeTile;
+    @FXML
+    private ImageView thirdPopeTile;
+
+
+    public void initialize(){
+
+    }
 }
