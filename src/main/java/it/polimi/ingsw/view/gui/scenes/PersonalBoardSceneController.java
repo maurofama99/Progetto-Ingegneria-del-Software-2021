111e82374e7b7501430df81b4ff96645c526f779
Christian Fabio Grazian <10629810@polimi.it>
2021-05-20 13:29:51 +0200

    Updated View
    Personal Board, Market and Dev Card Deck in progress

--- src/main/java/it/polimi/ingsw/view/gui/scenes/PersonalBoardSceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/PersonalBoardSceneController.java
@@ -1,7 +1,9 @@
 package it.polimi.ingsw.view.gui.scenes;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class PersonalBoardSceneController implements GenericSceneController {
     @FXML
@@ -30,9 +32,72 @@ public class PersonalBoardSceneController implements GenericSceneController {
     private ImageView secondPopeTile;
     @FXML
     private ImageView thirdPopeTile;
+    @FXML
+    private Button switchBtn;
+    @FXML
+    private Button baseProdBtn;
+    @FXML
+    private Button prod1Btn;
+    @FXML
+    private Button prod2Btn;
+    @FXML
+    private Button prod3Btn;
+    @FXML
+    private Button showMarketBtn; //Popup needed
+    @FXML
+    private Button showDevCardsBtn; //Popup needed
+    @FXML
+    private Button leader1Btn; //Popup needed
+    @FXML
+    private Button leader2Btn; //Popup needed
 
 
     public void initialize(){
+
+        switchBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, this::whenSwitchBtnClicked);
+        baseProdBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, this::whenBaseProdBtnClicked);
+        prod1Btn.addEventHandler(MouseEvent.MOUSE_CLICKED, this::whenProd1BtnClicked);
+        prod2Btn.addEventHandler(MouseEvent.MOUSE_CLICKED, this::whenProd2BtnClicked);
+        prod3Btn.addEventHandler(MouseEvent.MOUSE_CLICKED, this::whenProd3BtnClicked);
+        showDevCardsBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, this::whenShowDevCardsBtnClicked);
+        showMarketBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, this::whenShowMarketBtnClicked);
+        leader1Btn.addEventHandler(MouseEvent.MOUSE_CLICKED, this::whenLeader1BtnClicked);
+        leader2Btn.addEventHandler(MouseEvent.MOUSE_CLICKED, this::whenLeader2BtnClicked);
+    }
+
+    private void whenSwitchBtnClicked(MouseEvent event){
+
+    }
+
+    private void whenBaseProdBtnClicked(MouseEvent event){
+
+    }
+
+    private void whenProd1BtnClicked(MouseEvent event){
+
+    }
+
+    private void whenProd2BtnClicked(MouseEvent event){
+
+    }
+
+    private void whenProd3BtnClicked(MouseEvent event){
+
+    }
+
+    private void whenShowDevCardsBtnClicked(MouseEvent event){
+
+    }
+
+    private void whenShowMarketBtnClicked(MouseEvent event){
+
+    }
+
+    private void whenLeader1BtnClicked(MouseEvent event){
+
+    }
+
+    private void whenLeader2BtnClicked(MouseEvent event){
 
     }
 }
