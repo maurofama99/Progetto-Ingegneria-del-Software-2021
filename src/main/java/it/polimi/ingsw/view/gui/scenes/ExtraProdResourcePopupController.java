ce60b5d524205c66e00d2e5f958d715b0c2721b5
maurofama99 <mauro.fama@mail.polimi.it>
2021-06-23 22:05:16 +0200

    Fixed minor issues
    fixed bug in ExtraProdResourcePopupController

--- src/main/java/it/polimi/ingsw/view/gui/scenes/ExtraProdResourcePopupController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/ExtraProdResourcePopupController.java
@@ -1,16 +1,13 @@
 package it.polimi.ingsw.view.gui.scenes;
 
-import it.polimi.ingsw.model.resources.Resource;
 import it.polimi.ingsw.network.messagescs.ActivateExtraProd;
-import it.polimi.ingsw.network.messagescs.ResourceTypeChosen;
 import it.polimi.ingsw.observerPattern.ClientObservable;
 import it.polimi.ingsw.view.gui.SceneController;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -25,13 +22,7 @@ public class ExtraProdResourcePopupController extends ClientObservable implement
     @FXML
     private BorderPane rootPane;
     @FXML
-    private Button coin;
-    @FXML
-    private Button stone;
-    @FXML
-    private Button servant;
-    @FXML
-    private Button shield;
+    private ImageView coin, stone, servant, shield;
 
     public ExtraProdResourcePopupController(){
         stage = new Stage();
