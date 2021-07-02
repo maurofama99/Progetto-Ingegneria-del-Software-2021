1d4e07b18a161a9db8ca33cd74196fa0ee8ae352
valeria <valeria.detomas@mail.polimi.it>
2021-07-02 18:11:53 +0200

    Clean up

--- src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
@@ -7,17 +7,13 @@ import it.polimi.ingsw.model.player.leadercards.Discount;
 import it.polimi.ingsw.model.player.leadercards.LeaderCard;
 import it.polimi.ingsw.model.resources.Resource;
 import it.polimi.ingsw.model.resources.ResourceType;
-import it.polimi.ingsw.network.client.Client;
-import it.polimi.ingsw.network.messagescs.GoingMarket;
 import it.polimi.ingsw.network.messagescs.ResourcePlacement;
 import it.polimi.ingsw.network.messagescs.ResourceTypeChosen;
 import it.polimi.ingsw.network.server.ClientHandler;
 import it.polimi.ingsw.network.server.Server;
 import it.polimi.ingsw.view.VirtualView;
-import it.polimi.ingsw.view.cli.Cli;
 
 import java.io.OutputStream;
-import java.util.Scanner;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -33,15 +29,12 @@ public class GameControllerTest {
 
     private GameController gameController;
     private WaitingRoom waitingRoom;
-    private PlayerController playerController;
-    private HashMap<String, VirtualView> vvMap = new HashMap<>();
-    private ClientHandler clientHandler;
-    private ObjectOutputStream objectOutputStream;
+    private final HashMap<String, VirtualView> vvMap = new HashMap<>();
 
 
     @Before
     public void setUp() throws IOException {
-        clientHandler = new ClientHandler(new Server(waitingRoom, 1269), new Socket(), waitingRoom);
+        ClientHandler clientHandler = new ClientHandler(new Server(waitingRoom, 1269), new Socket(), waitingRoom);
         gameController = new GameController();
         waitingRoom = new WaitingRoom();
         ArrayList<Player> players = new ArrayList<>();
@@ -57,10 +50,10 @@ public class GameControllerTest {
         VirtualView vv = new VirtualView(clientHandler);
         vvMap.put(gameController.getTable().getCurrentPlayer().getNickname(), vv);
         gameController.setVvMap(vvMap);
-        playerController = new PlayerController(gameController);
+        PlayerController playerController = new PlayerController(gameController);
         gameController.setPlayerController(playerController);
 
-        objectOutputStream = new ObjectOutputStream(new OutputStream() {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new OutputStream() {
             @Override
             public void write(int b) throws IOException {
 
@@ -127,11 +120,6 @@ public class GameControllerTest {
     }
 
     @Test
-    public void testEndGameDevCard(){
-
-    }
-
-    @Test
     public void hasDiscountEffect(){
         ArrayList<Resource> requirements = new ArrayList<>();
         ArrayList<Color> colors = new ArrayList<>();
@@ -147,14 +135,4 @@ public class GameControllerTest {
         gameController.getPlayerController().hasDiscountEffect(requirements);
         assertEquals(prev, requirements.get(1).getQnt()+1);
     }
-
-    @Test
-    public void addFaithPointsToOpponent(){
-        gameController.setSinglePlayer(true);
-    }
-
-
-
-
-
 }
